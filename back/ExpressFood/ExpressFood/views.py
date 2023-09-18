6f6513ed9b16a6c66bb7a6c8d9739478f2014081
pascal-rithi-ke <pascal.rithi@gmail.com>
2023-09-18 17:03:01 +0200

    ajout de la route get_plats

--- back/ExpressFood/ExpressFood/views.py
+++ back/ExpressFood/ExpressFood/views.py
@@ -2,45 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 from dotenv import load_dotenv
+from bson import ObjectId
+
 import pymongo
 import os
-
+import json
 
 load_dotenv()
 
 user = os.getenv('MONGO_USER')
 password = os.getenv('MONGO_PASSWORD')
 host = os.getenv('MONGO_HOST')
+
 db_name = os.getenv('MONGO_DB_NAME')
+db_collection = os.getenv('MONGO_DB_COLLECTION')
 
 uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority"
-print(uri)
 
-client = pymongo.MongoClient(settings.MONGO_URI)
-database = client[settings.MONGO_DB_NAME]
+def get_plats(request):    
+    # On se connecte à la base de données
+    client = pymongo.MongoClient(uri)
+    db = client[db_name]
+    collection = db[db_collection]
 
-# Create your views here.
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-print(client)
-print(database)
-
-def get_plats(request):
-    plats_collection = database["plats"]
-
-    # Compter le nombre de documents dans la collection
-    num_plats = plats_collection.count_documents({})
-
-    # Afficher le nombre de plats dans la console (pour le débogage)
-    print(f"Nombre de plats dans la collection 'plats': {num_plats}")
-
-    # Renvoyer une réponse JSON avec le nombre de plats
-    return JsonResponse({"nombre_de_plats": num_plats})
-
-
-# myclient = pymongo.MongoClient("mongodb+srv://admin:adminpasswordMongoDB77@cluster0.sp2ay.mongodb.net/")
-# mydb = myclient["ExpressFood"]
-# mycol = mydb["plats"]
-
-# for x in mycol.find():
-#   print(x)
+    # Conversion ObjectID to str pour chaque document
+    plats = list(collection.find())
+    for plat in plats:
+        plat['_id'] = str(plat['_id'])
+    
+    # On renvoie les plats au format JSON
+    return JsonResponse(plats, safe=False)
