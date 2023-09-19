0b07683490f35d7e208f3be15c09bd665f178812
bflorestal <yayou7710@hotmail.fr>
2023-09-20 00:41:28 +0200

    feat: added button props and changed styles

--- front/src/Components/atoms/Button.tsx
+++ front/src/Components/atoms/Button.tsx
@@ -1,5 +1,9 @@
-export const Button = ({ children }: { children: React.ReactNode }) => {
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode
+}
+
+export const Button = ({ children, ...rest }: ButtonProps) => {
     return (
-        <button className="px-6 py-3 rounded bg-secondary text-white" type="submit">{children}</button>
+        <button className="px-6 py-3 rounded bg-secondary hover:bg-green-800 text-white font-semibold transition-colors" {...rest}>{children}</button>
     )
 }
