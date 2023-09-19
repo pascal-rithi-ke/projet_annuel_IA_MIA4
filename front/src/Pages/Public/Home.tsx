2d564b4887b377a5701f8bc1aa2d84405a064787
bflorestal <yayou7710@hotmail.fr>
2023-09-20 00:37:00 +0200

    feat: homepage with hero section

--- front/src/Pages/Public/Home.tsx
+++ front/src/Pages/Public/Home.tsx
@@ -1,10 +1,21 @@
-
+import { Button } from "../../Components/atoms/Button"
 
 export const Home = () => {
-
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <main className="min-h-full">
+      <section id="hero" className="bg-[url('img/hero_bg.avif')] bg-no-repeat bg-cover">
+        <div className="flex justify-center items-start lg:items-center flex-col text-center min-h-screen max-w-xl w-full mx-auto">
+          <div className="space-y-10">
+            <h1 className="text-dark text-3xl lg:text-4xl font-semibold">ExpressFood</h1>
+            <div>
+              <span className="text-dark text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aspernatur necessitatibus tempora voluptatem amet maxime.</span>
+            </div>
+          </div>
+          <div className="mt-9">
+            <Button>Test</Button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
