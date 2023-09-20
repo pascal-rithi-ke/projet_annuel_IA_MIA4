5bbe7104968283f221d80eea73f68e6a4e0e0859
pascal-rithi-ke <pascal.rithi@gmail.com>
2023-09-21 01:21:32 +0200

    checkpoint resolution problem model django

--- back/django/ExpressFoodApp/models.py
+++ back/django/ExpressFoodApp/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Plats(models.Model):
-    name: models.CharField(max_length=100)
-    description: models.CharField(max_length=100)
-    price: models.FloatField()
-    quantity: models.IntegerField()
-    availability: models.BooleanField()
-    type: models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='')
+    description = models.CharField(max_length=100, default='')
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
+    availability = models.BooleanField(default=False)
+    type = models.CharField(max_length=100, default='')
     
 class Livreur(models.Model):
-    name: models.CharField(max_length=100)
-    availability: models.BooleanField()
+    name = models.CharField(max_length=100, default='')
+    availability = models.BooleanField(default=False)
 
 class Client(models.Model):
-    name: models.CharField(max_length=100)
-    email: models.CharField(max_length=100)
-    password: models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='')
+    email = models.CharField(max_length=100, default='')
+    password = models.CharField(max_length=100, default='')
 
 class Commande(models.Model):
-    date: models.DateField()
-    time: models.TimeField()
-    client: models.ForeignKey(Client, on_delete=models.CASCADE)
-    livreur: models.ForeignKey(Livreur, on_delete=models.CASCADE)
-    plat: models.ForeignKey(Plats, on_delete=models.CASCADE)
+    date = models.DateField(default='')
+    time = models.TimeField(default='')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    livreur = models.ForeignKey(Livreur, on_delete=models.CASCADE)
+    plat = models.ForeignKey(Plats, on_delete=models.CASCADE)
--- back/django/ExpressFoodApp/views.py
+++ back/django/ExpressFoodApp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse, HttpResponse
@@ -14,17 +13,25 @@ def index(request):
 #Plats
 @csrf_exempt
 def platApi(request):
+# get all plats
     if request.method == 'GET':
         plats = Plats.objects.all()
         plats_serializer = PlatsSerialize(plats, many=True)
         return JsonResponse(plats_serializer.data, safe=False)
+# get a plat
+    if request.method == 'GET':
+        plat = Plats.objects.get(id=id)
+        plat_serializer = PlatsSerialize(plat)
+        return JsonResponse(plat_serializer.data, safe=False)
+# add a plat
     elif request.method == 'POST':
         plats_data = JSONParser().parse(request)
         plats_serializer = PlatsSerialize(data=plats_data)
         if plats_serializer.is_valid():
             plats_serializer.save()
             return JsonResponse("Added Successfully", safe=False)
-        return JsonResponse("Failed to Add", safe=False)
+        return JsonResponse("Failed to Add")
+# update a plat
     elif request.method == 'PUT':
         plats_data = JSONParser().parse(request)
         plats = Plats.objects.get(id=plats_data['id'])
@@ -33,6 +40,7 @@ def platApi(request):
             plats_serializer.save()
             return JsonResponse("Updated Successfully", safe=False)
         return JsonResponse("Failed to Update")
+# delete a plat
     elif request.method == 'DELETE':
         plats_data = JSONParser().parse(request)
         plats = Plats.objects.get(id=plats_data['id'])
